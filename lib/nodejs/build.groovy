21319b5975f755b2040d541b336877bcf3441fed
CentOS <centos@centos7.linuxvmimages.local>
2020-09-26 09:47:07 -0400

    aa

--- lib/nodejs/build.groovy
+++ lib/nodejs/build.groovy
@@ -1,10 +1,4 @@
 void call() {
-    stage('Cloning Git') {
-      steps {
-        git 'https://github.com/sufpro/NodeSonarExample.git'
-      }
-    }
-        
     stage('Install dependencies') {
       steps {
         sh 'npm install'
