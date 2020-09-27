07ec26f8bbd8232200d97451daaaaae5a6cb3df4
CentOS <centos@centos7.linuxvmimages.local>
2020-09-27 06:42:11 -0400

    aa

--- lib/nodejs/build.groovy
+++ lib/nodejs/build.groovy
@@ -1,10 +1,7 @@
 void call() {
     stage('Install dependencies') {
 nodejs(){
-   sh sh "${tool('nodeproject')}/bin/npm install"
+   sh "${tool('nodeproject')}/bin/npm install"
 }
 } 
-    stage('Test') {
-sh("npm test")
-    }
 }
