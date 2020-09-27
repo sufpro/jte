6b3fa1d46f09a21f20b5d6b4fa26d535666ecb4c
CentOS <centos@centos7.linuxvmimages.local>
2020-09-27 17:29:30 -0400

    aa

--- lib/sonarqube/qc.groovy
+++ lib/sonarqube/qc.groovy
@@ -1,18 +1,11 @@
 void call() {
+
 node {
-stage('SonarQube analysis') {
-    def scannerHome = tool 'sonarqube-scanner';
+    env.NODEJS_HOME1 = "${tool 'sonarqube-scanner'}"
+    // on linux / mac
+    env.PATH="${env.NODEJS_HOME1}/bin:${env.PATH}"
     withSonarQubeEnv() { // If you have configured more than one global server connection, you can specify its name
-      println "aaaaaaaaa" + config.projectKey
-      sh "${scannerHome}/bin/sonar-scanner \
-           -Dsonar.projectKey=" + config.projectKey + " \
-           -Dsonar.sources=" + config.sources + " \
-           -Dsonar.tests=" + config.tests + " \
-           -Dsonar.inclusions=" + config.inclusions ?: '**' + " \
-           -Dsonar.test.inclusions=" + config.testInclusions + " \
-           -Dsonar.javascript.lcov.reportPaths=" + config.lcov + " \
-           -Dsonar.testExecutionReportPaths=" + config.testExecutionReportPath
-    }
-  }
+    println 'xxxx' + config.projectKey
+}
 }
 }
