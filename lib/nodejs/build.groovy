ad5a695cbb6c2feba7cb057c38892f9d9a247c8b
CentOS <centos@centos7.linuxvmimages.local>
2020-09-27 10:57:48 -0400

    as

--- lib/nodejs/build.groovy
+++ lib/nodejs/build.groovy
@@ -1,7 +1,7 @@
 void call() {
 
 node {
-    env.NODEJS_HOME = "${tool 'nodeproject'}"
+    env.NODEJS_HOME = "${tool 'nodeproject'}/bin"
     // on linux / mac
     env.PATH="${env.NODEJS_HOME};${env.PATH}"
     sh 'npm --version'
