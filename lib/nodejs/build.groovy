85eb1d9d7439ac2fcc1ea561898b26b0b770b671
CentOS <centos@centos7.linuxvmimages.local>
2020-09-27 11:53:06 -0400

    as

--- lib/nodejs/build.groovy
+++ lib/nodejs/build.groovy
@@ -4,6 +4,10 @@ node {
     env.NODEJS_HOME = "${tool 'nodeproject'}"
     // on linux / mac
     env.PATH="${env.NODEJS_HOME}/bin:${env.PATH}"
+stage('Cloning Git') {
+      steps {
+        git 'https://github.com/sufpro/NodeSonarExample.git'
+      }
 stage("install") {
     sh 'npm install'
 }
