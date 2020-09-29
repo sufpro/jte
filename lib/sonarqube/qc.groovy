a92fd0be9f180e91bdd7d499771167c4255505d7
CentOS <centos@centos7.linuxvmimages.local>
2020-09-29 08:35:23 -0400

    aa

--- lib/sonarqube/qc.groovy
+++ lib/sonarqube/qc.groovy
@@ -1,14 +1,10 @@
 void call() {
 
 node {
-    env.NODEJS_HOME1 = "${tool 'sonarqube-scanner'}"
-    env.PATH="${env.NODEJS_HOME1}/bin:${env.PATH}"
-    println 'xxxx' + config.qq
 
     println 'environment=' + config.env
 
     withSonarQubeEnv() { // If you have configured more than one global server connection, you can specify its name
-	 println env.NODEJS_HOME1
          //sh "${NODEJS_HOME1}/bin/sonar-scanner"
 	 sh "sonar-scanner"
 }
